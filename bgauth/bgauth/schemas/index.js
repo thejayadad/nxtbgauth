5d991fd4765032f0f1a514970cdc32de757334cb
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-27 15:08:55 -0400

    update

---
+++ bgauth/bgauth/schemas/index.js
@@ -0,0 +1,7 @@
+import comment from "./comment"
+import postedBy from "./postedBy"
+import user from "./user"
+import post from "./post"
+
+
+export const schemaTypes = [comment, postedBy, user, post ]
